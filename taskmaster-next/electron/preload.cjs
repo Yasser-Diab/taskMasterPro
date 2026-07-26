ff7b5b924e3079c16785fe3683dba11be59c2565
Yasser Diab <yasserdiabhassan@gmail.com>
2026-07-26 08:46:30 +0300

    Delete taskmaster-next directory

--- taskmaster-next/electron/preload.cjs
+++
@@ -1,9 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('taskmasterShell', {
-  platform: process.platform,
-});
-
-contextBridge.exposeInMainWorld('taskmasterPlatform', {
-  getActiveWindow: () => ipcRenderer.invoke('taskmaster:get-active-window'),
-});
